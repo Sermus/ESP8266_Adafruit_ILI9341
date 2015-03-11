9da4b1759019dc4a29c4b9f9bd6052a794ab531d
sermus <Sermus@users.noreply.github.com>
2015-03-11 03:06:37 +0000

    Вторая версия, с сокращенными задержками SPI

--- user/hspi.c
+++ user/hspi.c
@@ -10,8 +10,7 @@ DC GPIO2
 */
 
 #define __min(a,b) ((a > b) ? (b):(a))
-
-static uint32_t *spi_fifo;
+uint32_t *spi_fifo;
 
 void hspi_init(void)
 {
@@ -29,57 +28,16 @@ void hspi_init(void)
 	// time length HIGHT level = (CPU clock / 10 / 2) ^ -1,
 	// time length LOW level = (CPU clock / 10 / 2) ^ -1
 	WRITE_PERI_REG(SPI_FLASH_CLOCK(HSPI),
-	   ((1 & SPI_CLKDIV_PRE) << SPI_CLKDIV_PRE_S) |
-	   ((3 & SPI_CLKCNT_N) << SPI_CLKCNT_N_S) |
-	   ((1 & SPI_CLKCNT_H) << SPI_CLKCNT_H_S) |
-	   ((3 & SPI_CLKCNT_L) << SPI_CLKCNT_L_S));
+	   ((0 & SPI_CLKDIV_PRE) << SPI_CLKDIV_PRE_S) |
+	   ((7 & SPI_CLKCNT_N) << SPI_CLKCNT_N_S) |
+	   ((3 & SPI_CLKCNT_H) << SPI_CLKCNT_H_S) |
+	   ((7 & SPI_CLKCNT_L) << SPI_CLKCNT_L_S));
+
+	WRITE_PERI_REG(SPI_FLASH_CTRL1(HSPI), 0);
 
 	uint32_t regvalue = SPI_FLASH_DOUT;
     regvalue &= ~(BIT2 | SPI_FLASH_USR_ADDR | SPI_FLASH_USR_DUMMY | SPI_FLASH_USR_DIN | SPI_USR_COMMAND | SPI_DOUTDIN); //clear bit 2 see example IoT_Demo
 	WRITE_PERI_REG(SPI_FLASH_USER(HSPI), regvalue);
-}
-
-#define SPIFIFOSIZE 16 //16 words length
-
-inline void hspi_wait_ready()
-{
-	while (READ_PERI_REG(SPI_FLASH_CMD(HSPI))&SPI_FLASH_USR);
-}
-
-inline void hspi_prepare_tx(uint32_t bytecount)
-{
-	uint32_t bitcount = bytecount * 8 - 1;
-
-//	WRITE_PERI_REG(SPI_FLASH_USER1(HSPI), (bitcount & SPI_USR_OUT_BITLEN) << SPI_USR_OUT_BITLEN_S);
-	WRITE_PERI_REG(SPI_FLASH_USER1(HSPI),
-			( (bitcount & SPI_USR_OUT_BITLEN) << SPI_USR_OUT_BITLEN_S ) |
-			( (bitcount & SPI_USR_DIN_BITLEN) << SPI_USR_DIN_BITLEN_S ) );
-}
-
-inline void hspi_start_tx()
-{
-	SET_PERI_REG_MASK(SPI_FLASH_CMD(HSPI), SPI_FLASH_USR);   // send
-}
-
-void hspi_send_uint8(uint8_t data)
-{
-	hspi_prepare_tx(1);
-	*spi_fifo = data;
-	hspi_start_tx();
-}
-
-void hspi_send_uint16(uint16_t data)
-{
-	hspi_prepare_tx(2);
-	*spi_fifo = data;
-	hspi_start_tx();
-}
-
-void hspi_send_uint32(uint32_t data)
-{
-	hspi_prepare_tx(4);
-	*spi_fifo = data;
-	hspi_start_tx();
 }
 
 void hspi_send_uint16_r(uint16_t data, uint32_t repeats)
